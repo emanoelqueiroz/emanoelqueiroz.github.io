233fadea97fc020bd05717fadade20a2260c3797
Emanoel Queiroz <emanoelquerioz@gmail.com>
2014-05-31 21:49:32 -0300

    add Editor de texto V0.0.1

---
+++ EditorDeTexto/js/script.js
@@ -0,0 +1,9 @@
+var negrito = function () {
+	document.execCommand('bold');
+}
+var italico = function () {
+	document.execCommand('italic');
+}
+var sublinhado = function () {
+	document.execCommand('underline');
+}
