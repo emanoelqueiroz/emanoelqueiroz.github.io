226df9ed2cf486e144789f528c66dbae28057699
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2014-06-11 11:40:10 -0300

    v0.0.3

--- EditorDeTexto/js/script.js
+++ EditorDeTexto/js/script.js
@@ -1,14 +1,16 @@
 window.onload = function() {
-	foco();
 	var papel = document.getElementById("papel");
 	papel.style.marginTop = '25px';
+	foco();
 };
+
 var foco = function () {
 	document.getElementById("papel").focus();
 }
+
 var ativados = function (botao) {
 	var ativ = document.getElementById(botao);
-	if(ativ.style.backgroundColor == 'rgb(102, 102, 102)' || ativ.style.backgroundColor == ''){
+	if(ativ.style.backgroundColor == 'rgb(36, 36, 36)' || ativ.style.backgroundColor == ''){
 		ativ.style.backgroundColor = '#FFF';
 		ativ.style.color = '#242424';
 		foco();
@@ -18,24 +20,46 @@ var ativados = function (botao) {
 		foco();
 	}
 }
+
+var alinhamento = function (tipo) {
+	var papel = document.getElementById("papel");
+	if (tipo === "left") {
+		papel.style.textAlign = 'left';
+		ativados('left');
+		foco();
+	}else if(tipo === "center"){
+		papel.style.textAlign = 'center';
+		ativados('center');
+		foco();
+	}else if(tipo === "right"){
+		papel.style.textAlign = 'right';
+		ativados('right');
+		foco();
+	};
+}
+
 var negrito = function () {
 	document.execCommand('bold');
 	ativados('negrito');
 }
+
 var italico = function () {
 	document.execCommand('italic');
 	ativados('italico');
 }
+
 var sublinhado = function () {
 	document.execCommand('underline');
 	ativados('sublinhado');
 }
+
 var editorFonte = function (fontName) {
     document.execCommand("fontName", false, fontName);
     var papel = document.getElementById('papel');
     papel.style.fontFamily = fontName;
     foco();
 }
+
 var smallCaps = function () {
 	var papel = document.getElementById('papel');
 	if(papel.style.fontVariant == 'small-caps'){
@@ -46,6 +70,7 @@ var smallCaps = function () {
 		ativados('smallCaps');
 	}
 }
+
 window.onkeydown = function(event) {
    if (event.keyCode == 9) {
    		this.value += "    ";
