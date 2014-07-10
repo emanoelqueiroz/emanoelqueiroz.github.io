a32532a17a6563f4236d991aaee47cd3e45eec38
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2014-07-10 19:42:46 -0300

    abc

--- Material/js/script.js
+++ Material/js/script.js
@@ -1,3 +1,14 @@
+(function() {
+   var conteudo = document.getElementById('conteudo');
+	tamanho = window.innerHeight;
+	if(tamanho > 600){
+		conteudo.style.bottom = '-'+tamanho+'px';
+		conteudo.style.marginBottom = tamanho / 2 + 'px';
+	}else{
+		conteudo.style.bottom = '-600px';
+		conteudo.style.marginBottom = '100px';
+	}
+})();
 var circulo = function () {
 	var circulo = document.getElementById('circulo');
 	var conteudo = document.getElementById('conteudo');
@@ -10,7 +21,12 @@ var conteudo = function () {
 	var circulo = document.getElementById('circulo');
 	var conteudo = document.getElementById('conteudo');
 	var fundo = document.getElementById('fundo_modal');
+	tamanho = window.innerHeight;
 	circulo.style.bottom = '';
-	conteudo.style.bottom = '';
+	if(tamanho > 600){
+		conteudo.style.bottom = '-'+tamanho+'px';
+	}else{
+		conteudo.style.bottom = '-600px';
+	}
 	fundo_modal.style.display = '';
 }
