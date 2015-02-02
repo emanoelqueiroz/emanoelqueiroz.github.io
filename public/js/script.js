9b1fb6909e3add58f5719df656bf313b87ff306d
Emanoel A. de Queiroz <emanoelqueiroz@gmail.com>
2015-02-01 23:25:28 -0200

    Adicionado savant para testar

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,18 @@
-function initialize () {
-	var topo = document.getElementById('topo');
-	var a_foto = document.getElementById('a_foto');
-	var conteudo = document.getElementById('conteudo');
+var audio = document.getElementById('hammer');
+audio.play();
+audio.src('audio/hammer.mp3');
+audio.play();
 
-	topo.style.height = '150px';
+function savantSite (x, y) {
+	var body = document.getElementsByTagName('body')[0],
+		vario = document.createElement('div');
 
-	setTimeout(function() {
-		a_foto.style.bottom = '-75px';
-		a_foto.style.transform = 'rotate(360deg)';
-	}, 1000);
-
-	setTimeout(function() {
-		conteudo.style.opacity = '1';
-	}, 1250);
+	vario.setAttribute('class', 'vario');
+	vario.style.top = (y - 10) + 'px';
+	vario.style.left = (x - 30) + 'px';
+	body.appendChild(vario);
 }
 
-initialize();
+document.onmousemove = function (e) {
+	savantSite(e.clientX, e.clientY);
+}
