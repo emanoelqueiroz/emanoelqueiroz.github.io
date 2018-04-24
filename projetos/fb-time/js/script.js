f63ac3f43a52c24cb362f86c7ae2e8cbaf2d5926
Emanoel03 <emanoelqueiroz@gmail.com>
2018-04-24 18:41:55 -0300

    Code Update

--- projetos/fb-time/js/script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-	var divs = $('#page').html();	
-
-	for (var i = 0; i < 8; i++) {
-		$('#page').append(divs);
-	};
-})
--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,6 @@
-function initialize () {
- var topo = document.getElementById('topo');
- var a_foto = document.getElementById('a_foto');
- var conteudo = document.getElementById('conteudo');
-
- topo.style.height = '150px';
-
- setTimeout(function() {
- a_foto.style.bottom = '-75px';
- a_foto.style.transform = 'rotate(360deg)';
- }, 1000);
-
- setTimeout(function() {
- conteudo.style.opacity = '1';
- }, 1250);
-}
-
-initialize();
++function () {
+    const ageEle = document.querySelector('#age');
+    const now = new Date();
+    const actualYear = now.getFullYear();
+    age.innerHTML = actualYear - 1994;
+}();
